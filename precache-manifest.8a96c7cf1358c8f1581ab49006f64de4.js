169fbaa118d347d17ef276c938d8281ed3b4ace9
Deniz Sultanoğlu <34817296+denizsult@users.noreply.github.com>
2021-08-02 13:21:33 +0300

    build

---
+++ precache-manifest.8a96c7cf1358c8f1581ab49006f64de4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f595b888edc28c417c51aaceac17b2",
+    "url": "/assets/mobile.css"
+  },
+  {
+    "revision": "1851a17b0cc871740bc239d49b84524e",
+    "url": "/assets/style.css"
+  },
+  {
+    "revision": "fbe8959f2a1daf646cab4b16f27c4a13",
+    "url": "/assets/tablet.css"
+  },
+  {
+    "revision": "56fc170938a6273bf0eb",
+    "url": "/css/chunk-vendors.8e61cfb3.css"
+  },
+  {
+    "revision": "ac3e8ff40a721edd679cebb9ff9ca4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32577ce3f79eec93a3d3",
+    "url": "/js/app.410b1f19.js"
+  },
+  {
+    "revision": "56fc170938a6273bf0eb",
+    "url": "/js/chunk-vendors.9cd72b7f.js"
+  },
+  {
+    "revision": "05c635965469fdeb9481b140e3f28436",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
